337428e8e9294e5e4bc1f6a5e9c644a5c1d0bacb
vitalis <vitalismutwiri2gmail.com>
2023-01-17 12:47:42 +0300

    added interface and item class

---
+++ item.rb
@@ -0,0 +1,21 @@
+class Item
+  def initialize(genre, author, source, label, publish_date, archived:false)
+    @id=rand(1..30)
+    @genre= genre
+    @author=author
+    @source=source
+    @label=label
+    @publish_date=publish_date
+    @archived=archived
+  end
+
+  def can_be_archived?
+    publish_date > 10
+  end
+
+  def move_to_archive
+    can_be_archived? ? true : ''
+  end
+
+end
+
---
+++ main.rb
@@ -0,0 +1,61 @@
+class Main
+  def self.menu
+    puts " welcome to our catalog"
+    puts "\n"
+    puts "Input the index to execute one of the options"
+
+    @list={
+      1=>'list all books',
+      2=> 'List all music albums',
+      3=> 'List all movies',
+      4=> 'List of games',
+      5=> 'List all genres (e.g "Comedy", "Thriller")',
+      6=> 'List all labels (e.g. "Gift", "New")',
+      7=>'List all authors (e.g. "Stephen King")',
+      8=>'List all sources (e.g. "From a friend", "Online shop")',
+      9=>'Add a book',
+      10=>'Add a music album',
+      11=>'Add a movie',
+      12=>'Add a game',
+      13=>'exit',
+    }
+    puts "\n"
+    @list.each do  |index, item|
+      puts "#{index}, #{item}" 
+    end
+    Integer(gets.chomp)
+  end
+
+  loop do
+    case menu
+    when 1
+      puts "hello"
+    when 2
+      puts "hello2"
+    when 3
+      puts "hello2"
+    when 4
+      puts "hello2"
+    when 5
+      puts "hello2"
+    when 6
+      puts "hello2"
+    when 7
+      puts "hello2"
+    when 9
+      puts "hello2"
+    when 10
+      puts "hello2"
+    when 11
+      puts "hello2"
+    when 12
+      puts "hello2"
+    when 13
+      exit
+    end
+  end
+end
+
+def main
+  Main.New
+end
