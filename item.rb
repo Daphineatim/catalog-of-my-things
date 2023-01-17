f62d20773cd0bd0fa648f2d90157ece159af5c74
Daphineatim <hopedaphine19@gmail.com>
2023-01-17 21:36:08 +0300

    create book-class

--- item.rb
+++ item.rb
@@ -1,3 +1,19 @@
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  alias can_be can_be_archived?
+
+  def initialize(publisher, cover_state, publish_date, id = Random.rand(1..1000))
+    super(publish_date, id)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    @cover_state == 'bad' || can_be
+  end
+end
+
 class Item
   def initialize(genre, author, source, label, publish_date)
     @id = rand(1..30)
