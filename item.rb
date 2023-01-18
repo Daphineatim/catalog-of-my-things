552dca8f7ad133b6fd6c4206a2b988d8be1ff992
Daphineatim <hopedaphine19@gmail.com>
2023-01-18 14:08:21 +0300

    Modify item.rb

--- item.rb
+++ item.rb
@@ -32,4 +32,21 @@ class Item
   def can_be_archived?
     @publish_date > 10
   end
+
+  def add_label(label)
+    @label = label
+    label.add_item(label)
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_source(source)
+    @source = source
+  end
 end
