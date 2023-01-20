127fb98114f96925eadc87b8a842dded16d5704e
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-01-20 11:05:57 +0100

    Added test for labels

---
+++ spec/labels_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../label'
+require_relative '../item'
+
+describe Label do
+  context 'Given an instance of a book' do
+    before :each do
+      @labels = Label.new('gift', 'red')
+    end
+
+    it 'returns a new label' do
+      expect(@labels).to be_instance_of Label
+    end
+
+    it 'check correct title' do
+      @labels.title = 'gift'
+      expect(@labels.title).to eql 'gift'
+    end
+
+    it 'check correct title' do
+      @labels.color = 'red'
+      expect(@labels.color).to eql 'red'
+    end
+  end
+end
